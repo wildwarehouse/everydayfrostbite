b5200eec80b37f092849378d0ad9114357f00b19
Emory Merryman <emory.merryman@gmail.com>
2017-03-07 03:29:31 -0500

    kludgery about bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,4 +15,4 @@
 #   You should have received a copy of the GNU General Public License
 #   along with everydayfrostbite.  If not, see <http://www.gnu.org/licenses/>.
 
-gnome-terminal
+bash -c "gnome-terminal ${@} && bash"
